6112491c2665fa790965af16627d1be482c5b5d2
perepeluknastia1507@gmail.com <perepeluknastia1507@gmail.com>
2026-08-03 15:20:35 +0300

    fix bugs2

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface NotesLayoutProps {
   children: React.ReactNode;
-  modal: React.ReactNode;
+  modal?: React.ReactNode; 
 }
 
 export default function NotesLayout({ children, modal }: NotesLayoutProps) {
